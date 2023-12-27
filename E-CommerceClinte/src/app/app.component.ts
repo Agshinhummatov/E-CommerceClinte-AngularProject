ae85cd0f274983c5833d2dead45c1d54d1f0c3a2
Agshinhummatov <agshinshh@code.edu.az>
2023-12-27 19:31:09 +0400

    Toastr Service Fixed

--- E-CommerceClinte/src/app/app.component.ts
+++ E-CommerceClinte/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any;
 
 @Component({
@@ -9,5 +11,16 @@ declare var $: any;
 })
 export class AppComponent {
   title = 'E-CommerceClient';
+
+  constructor(private toasterService : CustomToastrService) {
+
+    toasterService.message("merhaba", "başlık", {
+      messageType : ToastrMessageType.Success,
+      position : ToastrPosition.TopCenter,
+   
+    });
+   }
+
 }
+
 
---
+++ E-CommerceClinte/src/app/services/ui/custom-toastr.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomToastrService {
+
+  constructor(private toastr :ToastrService) {}
+
+  message( message:string , title: string, toastrOptions: Partial<ToastrOptions>) 
+  {
+   
+    this.toastr[toastrOptions.messageType](message,title,{
+      positionClass : toastrOptions.position
+    });
+    
+  }
+
+
+
+}
+
+export class ToastrOptions{
+  messageType : ToastrMessageType;
+  position : ToastrPosition;
+
+}
+
+
+export enum ToastrMessageType{
+  Error = "error",
+  Info = "info",
+  Success = "success",
+  Warning = "warning"
+
+}
+
+export enum ToastrPosition{
+  TopCenter = "toast-top-center",
+  TopLeft = "toast-top-left",
+  TopRight = "toast-top-right",
+  BottomCenter = "toast-bottom-center",
+  BottomLeft = "toast-bottom-left",
+  BottomRight = "toast-bottom-right",
+  TopFullWidth = "toast-top-full-width",
+  BottomFullWidth = "toast-bottom-full-width",
+}
