bf95389509b1a78007d646a1a4286c0f203572be
Agshinhummatov <agshinshh@code.edu.az>
2024-05-24 03:29:16 -0700

    Fixed : SignalR Service, Test : SignalR Message products-hub Succesfully

--- E-CommerceClinte/src/app/admin/components/dashboards/dashboards.component.ts
+++ E-CommerceClinte/src/app/admin/components/dashboards/dashboards.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { AlertifyService, MessageType, Position } from '../../../services/admin/alertify.service';
 import { BaseComponent, SpinnerType } from '../../../base/base.component';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { SignalRService } from '../../../services/common/signalr.service';
+import { ReceiveFunctions } from '../../../constants/receive-functions';
+import { HubUrls } from '../../../constants/hubs-url';
 
 
 @Component({
@@ -13,15 +16,21 @@ import { NgxSpinnerService } from 'ngx-spinner';
 })
 export class DashboardsComponent extends BaseComponent implements OnInit {
 
-  constructor(private alertify : AlertifyService, spinner : NgxSpinnerService) {
+  constructor(private alertify : AlertifyService, spinner : NgxSpinnerService, private signalRService: SignalRService ) {
 
     super(spinner);
+    signalRService.start(HubUrls.ProductHub)
    }
 
   ngOnInit(): void {
 
   //  this.showSpinner(SpinnerType.ballScaleMultiple);
-
+  this.signalRService.on(ReceiveFunctions.ProductAddedMessageReceiveFunction, message => {
+    this.alertify.message(message, {
+      messageType: MessageType.Notify,
+      position: Position.TopRight
+    })
+  });
    
   }
 
